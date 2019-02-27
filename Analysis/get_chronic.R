0047e5cc42fab8be10648328e5d60821678792fd
yhejazi <yhejazi@CORP.AMAZONWORKSPACES.COM>
2019-02-27 00:45:36 -0800

    Created get_chronic.R, modified setup

---
+++ Analysis/get_chronic.R
@@ -0,0 +1,49 @@
+diabetes <- med_revise %>% 
+  filter(grepl('Diabetes', Primary.ICD.Rollup)) %>%
+  mutate(label = 'Diabetes')
+
+hypertension <- med_revise %>% 
+  filter(grepl('I10', Primary.ICD.Diagnosis.Code)) %>%
+  mutate(label = 'Hypertension')
+
+obesity <- med_revise %>% 
+  filter(grepl('E66', Primary.ICD.Diagnosis.Code)) %>%
+  mutate(label = 'Obesity')
+
+cancer <- med_revise %>% 
+  filter(grepl('Cancer', Primary.ICD.Rollup)) %>%
+  mutate(label = 'Cancer')
+
+musculoskeletal <- med_revise %>% 
+  filter(grepl('musculoskeletal', Primary.ICD.Rollup)) %>%
+  mutate(label = 'Musculoskeletal')
+
+cardiovascular <- med_revise %>% 
+  filter(grepl('heart', Primary.ICD.Rollup)) %>%
+  mutate(label = 'Cardiovascular')
+
+asthma <- med_revise %>% 
+  filter(grepl('Asthma', Primary.ICD.Rollup)) %>%
+  mutate(label = 'Asthma')
+
+copd <- med_revise %>% 
+  filter(grepl('Chronic obstructive pulmonary disease', Primary.ICD.Rollup)) %>%
+  mutate(label = 'COPD')
+
+cholesterol <- med_revise %>% 
+  filter(grepl('E78', Primary.ICD.Diagnosis.Code)) %>%
+  mutate(label = 'Cholesterol')
+
+# Primary.ICD.Rollup unknown
+sleep_disorders <- med_revise %>% 
+  filter(grepl('G47', Primary.ICD.Diagnosis.Code)) %>%
+  mutate(label = 'Sleep Disorders')
+
+# Primary.ICD.Rollup unknown
+pain <- med_revise %>% 
+  filter(grepl('R52', Primary.ICD.Diagnosis.Code))  %>%
+  mutate(label = 'Pain')
+
+injury <- med_revise %>% 
+  filter(grepl('injury', Primary.ICD.Rollup)) %>%
+  mutate(label = 'Injury')
